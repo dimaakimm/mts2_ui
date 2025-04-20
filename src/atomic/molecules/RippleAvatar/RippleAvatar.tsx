6a20f52cdbfea2953be074276b4ec12e4fe1b2d1
Demos <166336733+Demos-gloryofRome44@users.noreply.github.com>
2025-04-20 13:32:42 +0300

    Demos-02: refactor rippleAvatat

--- src/atomic/molecules/RippleAvatar/RippleAvatar.tsx
+++ src/atomic/molecules/RippleAvatar/RippleAvatar.tsx
@@ -5,27 +5,34 @@ import styles from './RippleAvatar.module.scss'
 export interface RippleAvatarProps {
     size?: number
     updateInterval?: number
+    isActive?: boolean 
 }
 
 const RippleAvatar: React.FC<RippleAvatarProps> = ({
     updateInterval = 250,
+    isActive = false, 
 }) => {
     const [ringCount, setRingCount] = useState<number>(3)
+    
     useEffect(() => {
         const id = setInterval(() => {
-            setRingCount(Math.floor(Math.random() * 3) + 1)
+            setRingCount(isActive ? Math.floor(Math.random() * 3) + 1 : 0)
         }, updateInterval)
         return () => clearInterval(id)
-    }, [updateInterval])
+    }, [updateInterval, isActive])
 
     return (
-        <div className={styles.rippleAvatar}>
+        <div className={`${styles.rippleAvatar} ${isActive ? styles.active : ''}`}>
             <div className={styles.rippleAvatarCenter}>
                 <OperatorSvg className={styles.icon} />
             </div>
-            {ringCount > 2 && <div className={styles.highSound} />}
-            {ringCount > 1 && <div className={styles.midSound} />}
-            {ringCount > 0 && <div className={styles.lowSound} />}
+            {isActive && (
+                <>
+                    {ringCount > 2 && <div className={styles.highSound} />}
+                    {ringCount > 1 && <div className={styles.midSound} />}
+                    {ringCount > 0 && <div className={styles.lowSound} />}
+                </>
+            )}
         </div>
     )
 }
